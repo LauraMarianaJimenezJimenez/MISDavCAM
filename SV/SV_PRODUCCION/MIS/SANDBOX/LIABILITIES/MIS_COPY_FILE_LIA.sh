de53e19680f8a2596c53e09d31c3aa4761ffc461
Laura Mariana Jiménez <69278768+LauraMarianaJimenezJimenez@users.noreply.github.com>
2023-04-25 17:55:04 -0500

    Add files via upload

---
+++ SV/SV_PRODUCCION/MIS/SANDBOX/LIABILITIES/MIS_COPY_FILE_LIA.sh
@@ -0,0 +1,62 @@
+#----- PROCESO DE COPIA DE ARCHIVOS FUENTE A CARPETAS PROPIAS DEL MIS -----
+
+#--- Impresión en Consola del nombre del proceso ---
+echo 'Inicio copia de archivos de Pasivos'
+
+for i in "$@" 
+do
+case $i in
+    usuario_oozie=*)
+    USUARIO_OOZIE="${i#*=}"
+    shift
+    ;;
+    usuario_dominio=*)
+    USUARIO_DOMINIO="${i#*=}"
+    shift
+    ;;
+    periodo=*)
+    PERIODO="${i#*=}"
+    shift
+    ;;
+    ruta_origen_pasivos=*)
+    ruta_origen_pasivos="${i#*=}"
+    shift
+    ;;
+    ruta_origen_rentabilidad=*)
+    ruta_origen_rentabilidad="${i#*=}"
+    shift
+    ;;
+    ruta_fuentes_pasivos=*)
+    ruta_fuentes_pasivos="${i#*=}"
+    shift
+    ;;
+    *)
+    shift
+    ;;
+esac
+done
+
+#--- Definición de variables de Fecha ---
+D=${PERIODO}
+DAY=$(date -d "$D" '+%d')
+MONTH=$(date -d "$D" '+%m')
+YEAR=$(date -d "$D" '+%Y')
+
+#--- Impresión de verificación de Fecha ---
+echo "Fecha: $D"
+echo "Day: $DAY"
+echo "Month: $MONTH"
+echo "Year: $YEAR"
+
+#--- Creación de variables de ruta origen y destino para el proceso de copia ---
+export RUTA_ORIGEN=${ruta_origen_pasivos}
+export RUTA_ORIGEN_RENTABILIDAD=${ruta_origen_rentabilidad}
+export RUTA_DESTINO=${ruta_fuentes_pasivos}
+
+#--- Solicitud del Ticket de Kerberos para autenticación con el sistema ---
+kinit -kt ${USUARIO_OOZIE}.keytab ${USUARIO_OOZIE}@${USUARIO_DOMINIO}
+
+#--- Ejecución de la acción de copia de archivos, removiendo primero el archivo si existe ---
+
+hdfs dfs -rm -skipTrash $RUTA_DESTINO/CNTRLRTE.CSV
+hdfs dfs -cp $RUTA_ORIGEN_RENTABILIDAD/CNTRLRTE_$YEAR$MONTH$DAY.csv $RUTA_DESTINO/CNTRLRTE.CSV
