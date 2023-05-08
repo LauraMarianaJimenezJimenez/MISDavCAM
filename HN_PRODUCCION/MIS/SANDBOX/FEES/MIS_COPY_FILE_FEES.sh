a3db16f01d818b6f431a11fce2085edc7bf416c9
Laura Mariana Jiménez <69278768+LauraMarianaJimenezJimenez@users.noreply.github.com>
2023-05-08 16:19:30 -0500

    Add files via upload

---
+++ HN_PRODUCCION/MIS/SANDBOX/FEES/MIS_COPY_FILE_FEES.sh
@@ -0,0 +1,62 @@
+----- PROCESO DE COPIA DE ARCHIVOS FUENTE A CARPETAS PROPIAS DEL MIS -----
+
+--- Impresión en Consola del nombre del proceso ---
+echo 'Inicio copia de archivos fuente de Fuera de balance'
+
+for i in "$@" 
+do
+case $i in
+    usuario_oozie=*)
+    USUARIO_OOZIE="${i#*=}"
+    shift
+    ;;
+    usuario_dominio=*)
+    USUARIO_DOMINIO="${i#*=}"
+    shift
+    ;;
+    periodo=*)
+    PERIODO="${i#*=}"
+    shift
+    ;;
+    ruta_origen_comisiones=*)
+    ruta_origen_comisiones="${i#*=}"
+    shift
+    ;;
+    ruta_fuentes_comisiones=*)
+    ruta_fuentes_comisiones="${i#*=}"
+    shift
+    ;;
+    *)
+    shift
+    ;;
+esac
+done
+
+--- Definición de variables de Fecha ---
+D=${PERIODO}
+DAY=$(date -d "$D" '+%d')
+MONTH=$(date -d "$D" '+%m')
+YEAR=$(date -d "$D" '+%Y')
+
+--- Impresión de verificación de Fecha ---
+echo "Fecha: $D"
+echo "Day: $DAY"
+echo "Month: $MONTH"
+echo "Year: $YEAR"
+
+--- Creación de variables de ruta origen y destino para el proceso de copia ---
+export RUTA_ORIGEN=${ruta_origen_comisiones}
+export RUTA_DESTINO=${ruta_fuentes_comisiones}
+
+--- Creación de ruta de variable PYTHON EGG CACHE, necesaria para la ejecución de acciones en HUE ---
+export PYTHON_EGG_CACHE=/tmp/impala-shell-python-egg-cache-${USER}
+
+--- Solicitud del Ticket de Kerberos para autenticación con el sistema ---
+kinit -kt ${USUARIO_OOZIE}.keytab ${USUARIO_OOZIE}@${USUARIO_DOMINIO}
+
+--- Ejecución de la acción de copia de archivos, removiendo primero el archivo si existe ---
+hadoop dfs -rm -skipTrash $RUTA_DESTINO/CDIF03.csv
+hadoop dfs -cp $RUTA_ORIGEN/CDIF03_$YEAR$MONTH$DAY.csv $RUTA_DESTINO/CDIF03.csv
+
+hadoop dfs -rm -skipTrash $RUTA_DESTINO/CDIF07.csv
+hadoop dfs -cp $RUTA_ORIGEN/CDIF07_$YEAR$MONTH$DAY.csv $RUTA_DESTINO/CDIF07.csv
