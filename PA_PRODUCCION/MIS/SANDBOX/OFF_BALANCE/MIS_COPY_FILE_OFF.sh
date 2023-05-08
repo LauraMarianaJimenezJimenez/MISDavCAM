988d05831f03e2c94f7edad5bc197b2d6dd59fce
Laura Mariana Jiménez <69278768+LauraMarianaJimenezJimenez@users.noreply.github.com>
2023-05-08 16:57:33 -0500

    Create MIS_COPY_FILE_OFF.sh

---
+++ PA_PRODUCCION/MIS/SANDBOX/OFF_BALANCE/MIS_COPY_FILE_OFF.sh
@@ -0,0 +1,65 @@
+----- PROCESO DE COPIA DE ARCHIVOS FUENTE A CARPETAS PROPIAS DEL MIS -----
+
+--- Impresión en Consola del nombre del proceso ---
+echo 'Inicio copia de archivos fuente de Fuera de balance'
+
+for i in "$@" 
+do
+case $i in
+    usuario_oozie=*)
+    USUARIO_OOZIE="${i#*=}"
+    shift
+    ;;
+    usuario_dominio=*)
+    USUARIO_DOMINIO="${i#*=}"
+    shift
+    ;;
+    periodo=*)
+    PERIODO="${i#*=}"
+    shift
+    ;;
+    ruta_origen_prueba=*)
+    ruta_origen_prueba="${i#*=}"
+    shift
+    ;;
+    ruta_origen_contingencias=*)
+    ruta_origen_contingencias="${i#*=}"
+    shift
+    ;;
+    ruta_fuentes_contingencias=*)
+    ruta_fuentes_contingencias="${i#*=}"
+    shift
+    ;;
+    *)
+    shift
+    ;;
+esac
+done
+
+--- Definición de variables de Fecha ---
+D=${PERIODO}
+DAY=$(date -d "$D" '+%d')
+MONTH=$(date -d "$D" '+%m')
+YEAR=$(date -d "$D" '+%Y')
+
+--- Impresión de verificación de Fecha ---
+echo "Fecha: $D"
+echo "Day: $DAY"
+echo "Month: $MONTH"
+echo "Year: $YEAR"
+
+--- Creación de variables de ruta origen y destino para el proceso de copia ---
+export RUTA_ORIGEN=${ruta_origen_prueba}
+
+#export RUTA_ORIGEN=${ruta_origen_contingencias}
+export RUTA_DESTINO=${ruta_fuentes_contingencias}
+
+--- Creación de ruta de variable PYTHON EGG CACHE, necesaria para la ejecución de acciones en HUE ---
+export PYTHON_EGG_CACHE=/tmp/impala-shell-python-egg-cache-${USER}
+
+--- Solicitud del Ticket de Kerberos para autenticación con el sistema ---
+kinit -kt ${USUARIO_OOZIE}.keytab ${USUARIO_OOZIE}@${USUARIO_DOMINIO}
+
+--- Ejecución de la acción de copia de archivos, removiendo primero el archivo si existe ---
+hadoop dfs -rm -skipTrash $RUTA_DESTINO/LCMST.CSV
+hadoop dfs -cp $RUTA_ORIGEN/LCMST_$YEAR$MONTH$DAY.CSV $RUTA_DESTINO/LCMST.CSV
