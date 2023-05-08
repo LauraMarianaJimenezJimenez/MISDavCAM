628c225b0df679ea25cb3ed92a8d07a4066f246d
Laura Mariana Jiménez <69278768+LauraMarianaJimenezJimenez@users.noreply.github.com>
2023-05-08 16:37:17 -0500

    Add files via upload

---
+++ HN_PRODUCCION/MIS_MON/SANDBOX/PROVISIONS/MIS_COPY_FILE_PROV.sh
@@ -0,0 +1,68 @@
+----- PROCESO DE COPIA DE ARCHIVOS FUENTE A CARPETAS PROPIAS DEL MIS -----
+
+--- Impresión en Consola del nombre del proceso ---
+echo 'Inicio copia de archivos fuente de Fuera de balance'
+
+for i in "$@" 
+do
+case $i in
+    usuario_oozie=*)
+    USUARIO_OOZIE="${i#*=}"
+    shift
+    ;;
+    usuario_dominio=*)
+    USUARIO_DOMINIO="${i#*=}"
+    shift
+    ;;
+    periodo=*)
+    PERIODO="${i#*=}"
+    shift
+    ;;
+    ruta_origen_comisiones=*)
+    ruta_origen_comisiones="${i#*=}"
+    shift
+    ;;
+    ruta_fuentes_provisiones=*)
+    ruta_fuentes_provisiones="${i#*=}"
+    shift
+    ;;
+    *)
+    shift
+    ;;
+esac
+done
+
+--- Definición de variables de Fecha ---
+D=${PERIODO}
+MONTH=$(date -d "$D" '+%m')
+YEAR=$(date -d "$D" '+%Y')
+dateTemp=$(date -d "${YEAR}${MONTH}01 + 1 month")
+echo "tmp $dateTemp"
+D=$(date -d "$dateTemp - 1 day" "+%Y%m%d")
+
+DAY=$(date -d "$D" '+%d')
+MONTH=$(date -d "$D" '+%m')
+YEAR=$(date -d "$D" '+%Y')
+
+--- Impresión de verificación de Fecha ---
+echo "Fecha: $D"
+echo "Day: $DAY"
+echo "Month: $MONTH"
+echo "Year: $YEAR"
+
+--- Creación de variables de ruta origen y destino para el proceso de copia ---
+export RUTA_ORIGEN=${ruta_origen_comisiones}
+export RUTA_DESTINO=${ruta_fuentes_provisiones}
+
+--- Creación de ruta de variable PYTHON EGG CACHE, necesaria para la ejecución de acciones en HUE ---
+export PYTHON_EGG_CACHE=/tmp/impala-shell-python-egg-cache-${USER}
+
+--- Solicitud del Ticket de Kerberos para autenticación con el sistema ---
+kinit -kt ${USUARIO_OOZIE}.keytab ${USUARIO_OOZIE}@${USUARIO_DOMINIO}
+
+--- Ejecución de la acción de copia de archivos, removiendo primero el archivo si existe ---
+hadoop dfs -rm -skipTrash $RUTA_DESTINO/TBL_DISTRI_CTAS_NIFF.csv
+hadoop dfs -cp $RUTA_ORIGEN/TBL_DISTRI_CTAS_NIFF_$YEAR$MONTH$DAY.csv $RUTA_DESTINO/TBL_DISTRI_CTAS_NIFF.csv
+
+hadoop dfs -rm -skipTrash $RUTA_DESTINO/TBL_REPORTE_FINAL_HIST_BC.csv
+hadoop dfs -cp $RUTA_ORIGEN/TBL_REPORTE_FINAL_HIST_BC_$YEAR$MONTH$DAY.csv $RUTA_DESTINO/TBL_REPORTE_FINAL_HIST_BC.csv
